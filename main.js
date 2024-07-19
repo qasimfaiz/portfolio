31a2d18404fad79560e4e9072d4412786fa7b915
qasimfaiz <faizqasim670@gmail.com>
2024-07-19 18:40:56 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const counters = document.querySelectorAll('.counter');
+    const speed = 300; // The lower the slower
+  
+    counters.forEach(counter => {
+      const updateCount = () => {
+        const target = +counter.getAttribute('data-target');
+        const count = +counter.innerText;
+  
+        // Lower inc to slow and higher to slow
+        const inc = Math.ceil(target / speed);
+  
+        // Check if target is reached
+        if (count < target) {
+          // Add inc to count and output in counter
+          counter.innerText = count + inc;
+          // Call function every 1ms
+          setTimeout(updateCount, 1);
+        } else {
+          counter.innerText = target;
+        }
+      };
+  
+      updateCount();
+    });
+  });
+  
